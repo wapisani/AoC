8a4d20cf86dd8a1e8b5ddbafb256518b6c78dd92
Will Pisani <wapisani@mtu.edu>
2021-12-06 16:12:23 -0600

    Finished part 2, implemented rotation solution for part 1

--- 2021/day6.py
+++ 2021/day6.py
@@ -4,6 +4,7 @@
 """
 
 import os
+import numpy as np
 
 directory = r'F:\Documents\Programming\AoC\2021'
 os.chdir(directory)
@@ -14,22 +15,21 @@ with open('input_day6.txt','r') as handle:
 # with open('sample_day6.txt','r') as handle:
 #     data = handle.readlines()[0].split(',')
     
-### Part 1 ###
-fish = [int(value) for value in data]
-days = 80
+### Part 1 and 2 ###
+# Part 1 80 days, Part 2 256 days
+initial_fish = [int(value) for value in data]
+days = 256
+n = 9 # number of possible timer states
+fish_count = np.zeros(n)
+for fish in initial_fish:
+    fish_count[fish] += 1
+
 for day in range(days):
-    new_fish_count = 0
-    for index,timer in enumerate(fish):
+    new_fish_count = fish_count[0]
+    for i in range(1,n):
+        fish_count[i-1] = fish_count[i]
         
-        fish[index] -= 1
-        if fish[index] < 0:
-            fish[index] = 6
-            new_fish_count += 1
-        
-    # Add new fish here
-    for new_fish in range(new_fish_count):
-        fish.append(8)
+    fish_count[n-2-1] += new_fish_count
+    fish_count[n-1] = new_fish_count
 
-print(f'After {days} days, there are {len(fish)} fish.')
-
-### Part 2 ###
+print(f'After {days} days, there are {int(np.sum(fish_count))} fish.')
