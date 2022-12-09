42508337d1f5a21f33c76fb89adc0fbf6292ea2b
Will Pisani <wapisani@mtu.edu>
2022-12-08 21:46:55 -0600

    Forgot to remove sample data

--- 2022/day7.py
+++ 2022/day7.py
@@ -11,30 +11,6 @@ os.chdir(directory)
 
 with open('input_day7.txt','r') as handle:
     data = handle.readlines()
-    
-# data = """$ cd /
-# $ ls
-# dir a
-# 14848514 b.txt
-# 8504156 c.dat
-# dir d
-# $ cd a
-# $ ls
-# dir e
-# 29116 f
-# 2557 g
-# 62596 h.lst
-# $ cd e
-# $ ls
-# 584 i
-# $ cd ..
-# $ cd ..
-# $ cd d
-# $ ls
-# 4060174 j
-# 8033020 d.log
-# 5626152 d.ext
-# 7214296 k""".split('\n')
 
 # Part 1
 dir_sizes = {}
